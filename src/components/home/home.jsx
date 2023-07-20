49a012bebcacc8a595fe1fecf37eb9c0d49fb333
George Irungu <ngothogeorgedesign@gmail.com>
2023-07-20 11:06:43 +0300

    add navbar section

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -2,7 +2,20 @@ import React from "react";
 import "./home.css";
 
 const Home = () => {
-  return <div>home</div>;
+  return (
+    <div id='home'>
+      <div className='home-content'>
+        <p>Hello I'm</p>
+        <h2>George Irungu</h2>
+        <p>Fullstack developer</p>
+        <div className='home-content-btn'>
+          <input type='button' value='Download CV' />
+          <input type='button' value="Let's talk" />
+        </div>
+      </div>
+      <div className='home-image'></div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,8 +1,61 @@
 import React from "react";
 import "./navbar.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { BiSolidBookAlt } from "react-icons/bi";
+import { MdExplore } from "react-icons/md";
+import { AiFillSetting } from "react-icons/ai";
+import { BsBook } from "react-icons/bs";
+import { BsFillMicFill } from "react-icons/bs";
+import { BiSolidContact } from "react-icons/bi";
+import { BiFoodMenu } from "react-icons/bi";
 
 const Navbar = () => {
-  return <div>navbar</div>;
+  return (
+    <nav id='navbar'>
+      <ul>
+        <li>
+          <a href='#Home'>
+            <AiOutlineHome />
+          </a>
+        </li>
+        <li>
+          <a href='#About'>
+            <BiSolidBookAlt />
+          </a>
+        </li>
+        <li>
+          <a href='#Experience'>
+            <MdExplore />
+          </a>
+        </li>
+        <li>
+          <a href='#Services'>
+            <AiFillSetting />
+          </a>
+        </li>
+        <li>
+          <a href='#Projects'>
+            <BsBook />
+          </a>
+        </li>
+        <li>
+          <a href='#Testimonials'>
+            <BsFillMicFill />
+          </a>
+        </li>
+        <li>
+          <a href='#Contact'>
+            <BiSolidContact />
+          </a>
+        </li>
+        <li>
+          <a href='#Footer'>
+            <BiFoodMenu />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
