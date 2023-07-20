0d367f5d2a6f1d8abb16825fc8976d7d76c34585
George Irungu <ngothogeorgedesign@gmail.com>
2023-07-20 14:13:33 +0300

    add home section

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,19 +1,53 @@
 import React from "react";
 import "./home.css";
+import homeImage from "../../images/me.png";
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { AiOutlineGithub } from "react-icons/ai";
+import { AiOutlineInstagram } from "react-icons/ai";
 
 const Home = () => {
   return (
-    <div id='home'>
-      <div className='home-content'>
-        <p>Hello I'm</p>
-        <h2>George Irungu</h2>
-        <p>Fullstack developer</p>
-        <div className='home-content-btn'>
-          <input type='button' value='Download CV' />
-          <input type='button' value="Let's talk" />
+    <div className='home-section'>
+      <div id='home'>
+        <div className='home-content left'>
+          <div className='icons'>
+            <a href='#'>
+              <AiOutlineLinkedin />
+            </a>
+          </div>
+          <div className='icons'>
+            <a href='#'>
+              <AiOutlineGithub />
+            </a>
+          </div>
+          <div className='icons'>
+            <a href='#'>
+              <AiOutlineInstagram />
+            </a>
+          </div>
+          <div className='icons div'></div>
+        </div>
+
+        <div className='center'>
+          <div className='home-content'>
+            <p>Hello I'm</p>
+            <h2>George Irungu</h2>
+            <p>Fullstack developer</p>
+            <div className='home-content-btn'>
+              <input type='button' value='Download CV' id='btn-download' />
+              <input type='button' value="Let's talk" id='btn-talk' />
+            </div>
+          </div>
+
+          <div className='home-image'>
+            <img src={homeImage} />
+          </div>
+        </div>
+
+        <div className='home-content right'>
+          <a href='#'>scroll down</a>
         </div>
       </div>
-      <div className='home-image'></div>
     </div>
   );
 };
